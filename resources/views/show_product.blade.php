2b0a3c28f1550a65fb0a63ab2432be61249d7030
Gema Dodi Pranata <gemadp01@gmail.com>
2024-04-03 19:58:01 +0700

    add: add to cart

--- resources/views/show_product.blade.php
+++ resources/views/show_product.blade.php
@@ -19,6 +19,12 @@
     <form action="{{ route('edit_product', $product) }}" method="get">
         <button type="submit">Edit Product</button>
     </form>
+
+    <form action="{{ route('add_to_cart', $product) }}" method="post">
+        @csrf
+        <input type="number" name="amount" value="1">
+        <button type="submit">Add to Cart</button>
+    </form>
 </body>
 
 </html>
