5176c03ed4f9635dba396f0517c7fefc3af032ac
Gema Dodi Pranata <gemadp01@gmail.com>
2024-04-04 10:15:21 +0700

    add: styling blade

--- resources/views/show_product.blade.php
+++ resources/views/show_product.blade.php
@@ -1,38 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $product->name }}</title>
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Product Detail') }}</div>
 
-<body>
+                <div class="card-body">
+                    <div class="d-flex justify-content-around">
+                        <div class="">
+                            <img src="{{ url('storage/' . $product->image) }}" alt="" width="200px">
+                        </div>
+                        <div class="">
+                            <h1>{{ $product->name }}</h1>
+                            <h6>{{ $product->description }}</h6>
+                            <h3>Rp{{ $product->price }}</h3>
+                            <hr>
+                            <p>{{ $product->stock }} left</p>
+                            @if (!Auth::user()->is_admin)
+                            <form action="{{ route('add_to_cart', $product) }}" method="post">
+                                @csrf
+                                <div class="input-group mb-3">
+                                    <input type="number" class="form-control" aria-describedby="basic-addon2"
+                                        name="amount" value=1>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-outline-secondary" type="submit">Add to
+                                            cart</button>
+                                    </div>
+                                </div>
+                            </form>
+                            @else
+                            <form action="{{ route('edit_product', $product) }}" method="get">
+                                <button type="submit" class="btn btn-primary">Edit product</button>
+                            </form>
+                            @endif
+                        </div>
+                    </div>
 
-
-    <a href="{{ route('index_product') }}">Back to Index Product</a>
-    <p>Name: {{ $product->name }}</p>
-    <p>Description: {{ $product->description }}</p>
-    <p>Rp{{ $product->price }}</p>
-    <p>Stock: {{ $product->stock }}</p>
-    <img src="{{ url('storage/' . $product->image) }}" alt="" height="100px">
-
-    <form action="{{ route('edit_product', $product) }}" method="get">
-        <button type="submit">Edit Product</button>
-    </form>
-
-    <form action="{{ route('add_to_cart', $product) }}" method="post">
-        @csrf
-        <input type="number" name="amount" value="1">
-        <button type="submit">Add to Cart</button>
-    </form>
-
-    @if ($errors->any())
-    @foreach ($errors->all as $error)
-    <p>{{ $error }}</p>
-    @endforeach
-    @endif
-</body>
-
-</html>
+                    @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                    @endforeach
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/show_profile.blade.php
+++ resources/views/show_profile.blade.php
@@ -1,44 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
-</head>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Profile') }}</div>
 
-<body>
-    @if ($errors->any())
-    @foreach ($errors->all as $error)
-    <p>{{ $error }}</p>
-    @endforeach
-    @endif
+                <div class="card-body">
+                    @if ($errors->any())
+                    @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                    @endforeach
+                    @endif
 
-    <p>Name: {{ $user->name }}</p>
-    <p>Email: {{ $user->email }}</p>
-    <p>Role: {{ $user->is_admin ? 'Admin' : 'Member' }}</p>
-
-    <form action="{{ route('edit_profile', $user) }}" method="post">
-        @csrf
-        <br>
-        <label>Name</label>
-        <br>
-        <input type="text" name="name" value="{{ $user->name }}">
-
-        <br>
-        <label>Password</label>
-        <br>
-        <input type="password" name="password">
-
-        <br>
-        <label>Password Confirmation</label>
-        <br>
-        <input type="password" name="password_confirmation">
-
-        <br>
-        <button type="submit">Change Profile Details</button>
-    </form>
-</body>
-
-</html>
+                    <form action="{{ route('edit_profile') }}" method="post">
+                        @csrf
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" placeholder="Name" class="form-control"
+                                value="{{ $user->name }}">
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" value="{{ $user->email }}" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label>Role</label>
+                            <input type="role" class="form-control" value="{{ $user->is_admin ? 'Admin' : 'Member' }}"
+                                disabled>
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm password</label>
+                            <input type="password" name="password_confirmation" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary mt-3">Change profile details</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
